e5b822f4f7efe9aa34ad80e962e20453ca51fc95
Christian Taylor <christianbtaylor@gmail.com>
2017-08-03 13:47:45 -0500

    Link ending date

--- resources/assets/js/components/events/Event.vue
+++ resources/assets/js/components/events/Event.vue
@@ -4,12 +4,12 @@
       <time class="event-start" :datetime="event.start_time.format()" :title="event.start_time.format('dddd, MMMM Do YYYY, h:mm:ss a')">
         <span class="event-start-day-name">{{ event.start_time.format('ddd') }}</span>
         <span class="event-start-day">{{ event.start_time.format('MM D') }}</span>
-        <span class="event-start-time">{{ displayStartTime() }}</span>
+        <span class="event-start-time" v-html="displayStartTime()"></span>
       </time>
       <time class="event-end" :datetime="event.end_time.format()" :title="event.end_time.format('dddd, MMMM Do YYYY, h:mm:ss a')">
         <span class="event-end-day-name">{{ event.end_time.format('ddd') }}</span>
         <span class="event-end-day">{{ event.end_time.format('MM D') }}</span>
-        <span class="event-end-time">{{ displayEndTime() }}</span>
+        <span class="event-end-time" v-html="displayEndTime()"></span>
       </time>
     </div>
     <div class="event-section">
@@ -30,6 +30,7 @@
 
 <script>
 import moment from 'moment'
+import { formatUrlDate } from '../../helpers'
 
 export default {
   props: ['event'],
@@ -38,18 +39,18 @@ export default {
     displayStartTime () {
       if (this.event.starts_today) {
         return this.event.start_time.format('h:mm A')
+      } else if (this.event.ends_today) {
+        return 'Ends'
       } else {
-        if (this.event.ends_today) {
-          return 'Ends'
-        } else {
-          return 'All Day'
-        }
+        return 'All Day'
       }
     },
 
     displayEndTime () {
       if (this.event.ends_today) {
         return this.event.end_time.format('h:mm A')
+      } else if (this.event.starts_today) {
+        return `&rarr; <a href="/?start_time=${formatUrlDate(this.event.end_time)}">${this.event.end_time.format('M/D')}</a>`
       }
     }
   }
