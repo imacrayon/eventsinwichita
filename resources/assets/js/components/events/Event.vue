ff2d13e1289fc48dd678e299d418e081265f9663
Christian Taylor <christianbtaylor@gmail.com>
2017-08-03 20:37:16 -0500

    Filter events by date with ajax

--- resources/assets/js/components/events/Event.vue
+++ resources/assets/js/components/events/Event.vue
@@ -4,12 +4,25 @@
       <time class="event-start" :datetime="event.start_time.format()" :title="event.start_time.format('dddd, MMMM Do YYYY, h:mm:ss a')">
         <span class="event-start-day-name">{{ event.start_time.format('ddd') }}</span>
         <span class="event-start-day">{{ event.start_time.format('MM D') }}</span>
-        <span class="event-start-time" v-html="displayStartTime()"></span>
+        <span class="event-start-time" v-if="this.event.starts_today">
+          {{ event.start_time.format('h:mm A') }}
+        </span>
+        <span class="event-start-time" v-else-if="this.event.ends_today">
+          Ends
+        </span>
+        <span class="event-start-time" v-else>
+          All Day
+        </span>
       </time>
       <time class="event-end" :datetime="event.end_time.format()" :title="event.end_time.format('dddd, MMMM Do YYYY, h:mm:ss a')">
         <span class="event-end-day-name">{{ event.end_time.format('ddd') }}</span>
         <span class="event-end-day">{{ event.end_time.format('MM D') }}</span>
-        <span class="event-end-time" v-html="displayEndTime()"></span>
+        <span class="event-end-time" v-if="event.ends_today">
+          {{ event.end_time.format('h:mm A') }}
+        </span>
+        <span class="event-end-time" v-else-if="event.starts_today">
+          &rarr; <a @click="filterByDate(event.end_time)">{{ event.end_time.format('M/D') }}</a>
+        </span>
       </time>
     </div>
     <div class="event-section">
@@ -39,22 +52,8 @@ export default {
   props: ['event'],
 
   methods: {
-    displayStartTime () {
-      if (this.event.starts_today) {
-        return this.event.start_time.format('h:mm A')
-      } else if (this.event.ends_today) {
-        return 'Ends'
-      } else {
-        return 'All Day'
-      }
-    },
-
-    displayEndTime () {
-      if (this.event.ends_today) {
-        return this.event.end_time.format('h:mm A')
-      } else if (this.event.starts_today) {
-        return `&rarr; <a href="/?start_time=${formatUrlDate(this.event.end_time)}">${this.event.end_time.format('M/D')}</a>`
-      }
+    filterByDate (date) {
+      this.$root.filters.start_time = formatUrlDate(date)
     }
   }
 }
