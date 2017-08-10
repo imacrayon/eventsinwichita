e2f34a3368e9ca10b9d168d1b4bfa2fc10eb429d
Christian Taylor <christianbtaylor@gmail.com>
2017-08-10 10:28:02 -0500

    Rename Facebook token & add client token

--- config/collectors.php
+++ config/collectors.php
@@ -4,7 +4,7 @@ return [
     'facebook' => [
         // Requires user permissions to access events from pages that have an
         // 18+ age restriction.
-        'token' => env('FACEBOOK_TOKEN')
+        'token' => env('FACEBOOK_COLLECTOR_TOKEN')
     ],
     'geocoder' => [
         'token' => env('GOOGLE_SECRET')
