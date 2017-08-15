1f1740818b1e157b56fca079efeac8b2fa331caf
Christian Taylor <christianbtaylor@gmail.com>
2017-08-15 16:57:18 -0500

    Add error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('main')
+<div class="grid-x grid-padding-x splash-image bg-image">
+    <div class="cell small-12 text-center">
+
+        <h1>Nothing's here.</h1>
+        <div>Check out other <a href="{{ url('/') }}">upcoming events</a>.</div>
+
+    </div>
+</div>
+@endsection
---
+++ resources/views/errors/505.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('main')
+<div class="grid-x grid-padding-x splash-image bg-image">
+    <div class="cell small-12 text-center">
+
+        <h1>Our site is jacked up at the moment.</h1>
+        <div>Please try again later.</div>
+
+    </div>
+</div>
+@endsection
