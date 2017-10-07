3fbbc8915635b7b5794be62f1505766ca338179f
gempir <daniel.pasch.s@gmail.com>
2017-10-07 11:19:06 +0200

    fixed padding

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,12 +5,12 @@
     <div class="cell small-12">
 
         <div class="card">
-            <div class="card-section text-center">
+            <div class="card-section">
                 <a class="button small" style="background-color: #3b5999;" href="{{ url('/auth/facebook')}}" data-tooltip="Login with Facebook" tabindex="6">
                     <svg class="icon" style="font-size: 1.25em;vertical-align: bottom;"><use xlink:href="{{ asset('/images/icons.svg#icon-facebook') }}"></use></svg>
                     Login
                 </a>
-
+                <hr>
                 {{--
                 <a class="button" style="background-color: #55acee; padding: .5rem .75rem;" href="{{ url('/auth/twitter')}}" data-tooltip="Login with Twitter" tabindex="7">
                     <svg class="icon"><use xlink:href="#icon-twitter"></use></svg>
@@ -22,8 +22,6 @@
                     Login
                 </a>
                 --}}
-            </div>
-            <div class="card-foot">
                 <form role="form" method="POST" action="{{ url('/login') }}">
                     {{ csrf_field() }}
 
