fa45c3fa840ef3469d90731ba1f06d47c53ed11b
lacasera <aboateng62@gmail.com>
2019-10-04 20:18:45 +0000

    add events api endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace' => 'Api'], function (){
+    Route::get('events', 'EventController@index')->name('api.events.index');
+});
